b5e440959fba65985fd3db5e8f44f60a60727f96
Le Trung Dung <ekkophantoms123@gmail.com>
2023-10-09 11:44:00 +0700

    fix output movie

--- src/controller/movie.controller.js
+++ src/controller/movie.controller.js
@@ -4,69 +4,25 @@ const movieController = {
     addMovie: async (req, res) => {
         try {
             const newMovie = await movieRepository.addMovie(req.body);
-            return res.status(200).json({
-                message: 'Add a movie successfully!!!',
-                data: newMovie
-            });
+            return res.status(200).json(newMovie)
         } catch (error) {
-            return res.status(500).json({
-                message: 'Can not add a movie!!!',
-            });
+            return res.status(500).json(error);
         }
     },
     getAllMovie: async (req, res) => {
         try {
             const allMovies = await movieRepository.getAllMovie();
-            return res.status(200).json({
-                message: 'Get all movies successfully!!!',
-                data: allMovies
-            });
+            return res.status(200).json(allMovies);
         } catch (error) {
-            return res.status(500).json({
-                message: 'Can not get all movies!!!',
-            });
+            return res.status(500).json(error);
         }
     },
     addSeason: async (req, res) => {
         try {
             const seasonMovies = await movieRepository.addSeason(req.body);
-            return res.status(200).json({
-                message: 'Add season of movies successfully!!!',
-                data: seasonMovies
-            });
+            return res.status(200).json(`Thêm ${seasonMovies} thành công !`);
         } catch (error) {
-            return res.status(500).json({
-                message: 'Can not add season of movies!!!',
-            });
-        }
-    },
-    markMovie: async (req, resp) => {
-        try {
-            const { userID, movieID } = req.body;
-            const movie = await movieRepository.markMovie({
-                userID,
-                movieID,
-            });
-            return resp.status(200).json({
-                message: "Mark movie successfully",
-                data: movie,
-            });
-        } catch (error) {
-            return resp.status(500).json(error);
-        }
-    },
-    getMarkMovie: async (req, resp) => {
-        try {
-            const { userID } = req.body;
-            const movies = await movieRepository.getMarkedMovie({
-                userID,
-            });
-            return resp.status(200).json({
-                message: "Get marked movie successfully",
-                data: movies,
-            });
-        } catch (error) {
-            return resp.status(500).json(error);
+            return res.status(500).json(error);
         }
     },
 }
