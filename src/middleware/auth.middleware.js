b13cd873554ed7bdc0782334c5ff7f3143b85040
unknown <thanhlche160808@fpt.edu.vn>
2023-10-18 14:49:24 +0700

    fix: authentication

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -4,10 +4,22 @@ const listByPassURL = [
     '/api/user/login',
     '/api/user/create',
     '/api/user/refresh',
+    '/api/movie_season/home',
+    '/api/movie_season/getAll',
+    '/api/movie_season/view',
+    '/api/movie_season/detail/',
+    '/api/comment/movie',
+    '/api/movie_video/detail/',
+    '/api/rate/movie/',
+    '/api/movie_season/filter',
+    '/api/movie_season/type',
+    '/api/movie_season/hot',
+    '/api/movie_season/search',
+    '/api/type/getAll',
 ]
 
 function checkExistURL(url) {
-    const result = listByPassURL.find(u => u.toLocaleLowerCase().trim() == url.toLowerCase().trim())
+    const result = listByPassURL.find(u => url.toLowerCase().trim().startsWith(u.toLowerCase().trim()))
     if (result)
         return true
     else
@@ -23,14 +35,16 @@ const authenticate = (req, res, next) => {
 
     const tokenHeader = req.header("Authorization");
     if (!tokenHeader) return res.status(401).json({ message: "Unauthorized" });
+    console.log('Token header: ', tokenHeader);
+    // const tokenParts = tokenHeader.split(" ");
+    // console.log(tokenParts);
 
-    const tokenParts = tokenHeader.split(" ");
-    if (tokenParts.length !== 2 || tokenParts[0] !== "Bearer") {
-        return res.status(401).json({ message: "Invalid token format" });
-    }
-    const token = tokenParts[1];
+    // if (tokenParts.length !== 2 || tokenParts[0] !== "Bearer") {
+    //     return res.status(401).json({ message: "Invalid token format" });
+    // }
+    // const token = tokenParts[1];
     try {
-        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        const decoded = jwt.verify(tokenHeader, process.env.SECRET_KEY);
         if (Date.now() >= decoded.exp * 1000) {
             return res.status(400).json({ message: "Access token expired" });
         }
@@ -40,5 +54,6 @@ const authenticate = (req, res, next) => {
         return res.status(401).json({ message: "Unauthorized" });
     }
 }
+
 
 export default authenticate;
